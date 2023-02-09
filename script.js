f50a2cba9bf451ce4ec3a7edbbe48b525c1870a4
gael <76548719+gaelzarco@users.noreply.github.com>
2023-02-08 16:18:45 -0800

    Camera, scene and mesh created

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Scripts Functional")
+// Scene
+const scene = new THREE.Scene()
+
+// White Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'white' })
+const mesh = new THREE.Mesh(geometry, material)
+// Add it to the scene
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera( 75, sizes.width/ sizes.height )
+// Add it to the scene
+scene.add(camera)
